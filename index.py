c8cb02092e5681ed0e32db298d2110a22f3b6705
Danielle Carrick <dacarrick@gmail.com>
2025-01-15 11:50:33 -0500

    code cleanup

--- index.py
+++ index.py
@@ -13,19 +13,10 @@ for file in file_list:
     dfs.append(df)
 
 df = pd.concat(dfs)
-#print(df.head())
 
 # Ensure the 'started_at' column is in datetime format
 df['datetimeOld'] = pd.to_datetime(df['started_at'], errors='coerce')
 df['datetime'] = pd.to_datetime(df['started_at'], format='ISO8601')
-
-# def coerce_datetime(startedAt):
-#     return pd.to_datetime(startedAt, errors='coerce')
-
-# df['datetimeFuncDef'] = df['started_at'].apply(coerce_datetime)
-# df['datetimeLambda'].apply(lambda startedAt: pd.to_datetime(startedAt, errors='coerce'))
-# How many rides are taken each day
-#dailycounts = df.groupby(pd.Grouper(key='datetime', freq='D')).count()
 
 # Extract the date part from the 'datetime' column
 df['date'] = df['datetime'].dt.isocalendar().week
@@ -39,15 +30,14 @@ daily_counts = daily_counts.reset_index()
 # Rename columns for clarity
 daily_counts.columns.name = None  # Remove the name of the index
 
+
 def snapshot_data(df: pd.DataFrame, fnameStem: str):
     suffix = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     fname = f'{fnameStem}-{suffix}.csv'
     df.to_csv(fname, index=False)
 
-snapshot_data(daily_counts, 'daily_ride_counts')
-
 # Save to a new CSV file (optional)
-#daily_counts.to_csv('daily_ride_counts-1.csv', index=False)
+snapshot_data(daily_counts, 'daily_ride_counts')
 
 print(daily_counts)
 
